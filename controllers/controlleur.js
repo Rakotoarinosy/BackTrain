8b53a8bf82e3047074932f1a30a11a75d8917bf5
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-02-09 22:59:42 +0300

    controlleur reservation with nom of most peaple

--- controllers/controlleur.js
+++ controllers/controlleur.js
@@ -11,58 +11,74 @@ exports.getTrainReservation = async (req, res, next) => {
             throw new Error('Missing parameter');
         }
         let trainReservations = [];
-
         trainReservations = await prisma.trainReservation.findMany({
             where: {
-                trainId: idTrain,
-                OR: [
-                    {
-                        reservation: {
-                            start: idGare
-                        }
-                    },
-                    {
-                        reservation: {
-                            end: idGare
-                        }
-                    }
-                ]
+              trainId: idTrain,
+              OR: [
+                {
+                  reservation: {
+                    start: idGare
+                  }
+                },
+                {
+                  reservation: {
+                    end: idGare
+                  }
+                }
+              ]
             },
             include: {
-                reservation: true,
-                train: true,
+              reservation: {
+                include: {
+                  reservationTraveler: { // Utilisez la relation reservationTraveler pour accéder aux voyageurs associés à chaque réservation
+                    include: {
+                      traveler: true // Inclure les détails du voyageur
+                    }
+                  }
+                }
+              },
+              train: true,
             },
-        });
-
-        // res.json(trainReservations);
-        // console.log(trainReservations)
+          });
+          
+        // console.log("test : ", trainReservations)
         let rep = [];
 
-        trainReservations.map((element) => {
-            rep.push({
-                start: element.reservation.start,
-                end: element.reservation.end,
-                nump: element.reservation.numP
+        trainReservations.forEach((element) => {
+            const reservation = element.reservation;
+            console.log("reservation kely :" , trainReservations)
+            const travelers = reservation.reservationTraveler.map(rt => rt.traveler);
+            travelers.forEach(traveler => {
+                rep.push({
+                    reservation: element.reservationId,
+                    start: reservation.start,
+                    end: reservation.end,
+                    nump: reservation.numP,
+                    name: traveler.nom, // Ajouter le nom du voyageur à la réponse
+                    CIN: traveler.cin // Ajouter l'email du voyageur à la réponse
+                });
             });
         });
-
-
+        
         let repStart = 0;
         let repEnd = 0;
-
+        
         rep.map((element) => {
+            console.log(element);
             if (element.start === idGare) {
                 repStart = repStart + element.nump
-            } else if(element.end === idGare){
+            } else if (element.end === idGare) {
                 repEnd = repEnd + element.nump
-            }else {
-                console.log("aucun reservation ne correspant à ce train")
+            } else {
+                console.log("aucun reservation ne correspond à ce train")
             }
         });
         res.json({
             repStart,
-            repEnd
+            repEnd,
+            reservations: rep // Ajouter les détails des réservations et des voyageurs à la réponse
         });
+        
 
     } catch (error) {
         next(error);
